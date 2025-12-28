c1b45459fdabb925ac7c2f15117252ea87d9bfd2
Hnsek <henrique2001sousa@gmail.com>
2025-12-28 18:50:26 -0300

    feat: implements map component from mablibre (WIP)

---
+++ src/components/Map.tsx
@@ -0,0 +1,14 @@
+import { MapView } from "@maplibre/maplibre-react-native"
+import { ReactNode } from "react"
+
+type Props = {
+
+}
+
+export default () : ReactNode  => {
+  return <MapView
+    mapStyle="https://tiles.openfreemap.org/styles/liberty"
+  >
+    
+</MapView>
+}
