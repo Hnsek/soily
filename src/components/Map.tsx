9bdac4e446ef8b2cc0811909b03ea15bcff7270b
Hnsek <henrique2001sousa@gmail.com>
2025-12-30 09:12:45 -0300

    chore: adds children support for map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,14 +5,16 @@ import { twMerge } from "tailwind-merge";
 
 type Props = {
     className?: string;
+    children?: ReactNode
 }
 
-export default ({className} : Props) : ReactNode  => {
+export default ({className, children} : Props) : ReactNode  => {
   return <View className={twMerge(className, "w-full h-full")}>
     <MapView
       mapStyle="https://tiles.openfreemap.org/styles/liberty"
       style={[{height:"100%", width:"100%"}]}
       >
+      {children}
     </MapView>
   </View>
 
