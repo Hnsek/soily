bfaf8adeaa15bb3d85c3a2288a7b05dd7dfe49db
Hnsek <henrique2001sousa@gmail.com>
2025-12-30 09:09:13 -0300

    chore: creates container in map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,14 +1,18 @@
 import { MapView } from "@maplibre/maplibre-react-native"
-import { ReactNode } from "react"
+import { ReactNode } from "react";
+import { View } from "react-native";
 
 type Props = {
-
+    className?: string;
 }
 
-export default () : ReactNode  => {
-  return <MapView
-    mapStyle="https://tiles.openfreemap.org/styles/liberty"
-  >
-    
-</MapView>
+export default ({className} : Props) : ReactNode  => {
+  return <View>
+    <MapView
+      mapStyle="https://tiles.openfreemap.org/styles/liberty"
+      style={[{height:"100%", width:"100%"}]}
+      >
+    </MapView>
+  </View>
+
 }
