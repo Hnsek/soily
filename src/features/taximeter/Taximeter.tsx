de6c9af47399ec52e8fe882b8579abffd2ba4bc6
Hnsek <henrique2001sousa@gmail.com>
2025-12-31 17:05:15 -0300

    refactor: removes unused code in Taximeter

--- src/features/taximeter/Taximeter.tsx
+++ src/features/taximeter/Taximeter.tsx
@@ -2,26 +2,8 @@ import { SafeAreaView } from "react-native-safe-area-context"
 import { Map, MapCamera } from "../../components/Map"
 import useLocation from "../../hooks/useLocation"
 import { useCamera } from "../../hooks/useCameraRef"
-import { Loading } from "../../components/Loading"
-import { useEffect, useState } from "react"
-import { getCurrentPosition } from "../../services/location"
 import { Location } from "../../types/location"
 
-export const TaximeterScreen = () => {
-  const [location, setLocation] = useState<Location>()
-
-  useEffect(() => {
-    getCurrentPosition()
-      .then((position) => setLocation(position.location))
-  }, [])
-
-
-  if(!location){
-    return <Loading/>
-  }
-
-  return <Taximeter initialLocation = {location}/>
-}
 
 type TaximeterProps = {
   initialLocation:  Location
