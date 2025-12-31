88fe69fda851c698e91bbae144129038168b52e6
Hnsek <henrique2001sousa@gmail.com>
2025-12-31 15:12:56 -0300

    chore: removes unused code in taximeter component

--- src/features/taximeter/index.tsx
+++ src/features/taximeter/index.tsx
@@ -1,33 +1,17 @@
-import { Text } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Map, MapCamera } from "../../components/Map"
 import useLocation from "../../hooks/useLocation"
-import { useEffect, useRef } from "react"
-import { CameraRef } from "@maplibre/maplibre-react-native"
+import { useCamera } from "../../hooks/useCameraRef"
 
 
 export const Taximeter = () => {
   const { location } = useLocation()  
 
-  console.warn([location?.longitude, location?.latitude] )
-  
-  const cameraRef = useRef<CameraRef>(null)
-  
-  useEffect(() => {
-
-      if(!location){
-        return
-      }
-
-      cameraRef.current?.setCamera({
-        centerCoordinate: [location.longitude, location.latitude],
-        zoomLevel: 17
-      })
-  }, [location])
+  const { cameraRef } = useCamera(location)
 
   return <SafeAreaView>
       <Map>
-              <MapCamera ref={cameraRef}/>
+        <MapCamera ref={cameraRef}/>
       </Map>
 </SafeAreaView>
 }
