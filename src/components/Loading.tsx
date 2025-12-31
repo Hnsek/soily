80e1fd26546f2f1f70f6ed1980bf5aaa6386d73e
Hnsek <henrique2001sousa@gmail.com>
2025-12-31 16:08:15 -0300

    feat: creates loading screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,15 +1,17 @@
 import { ComponentPropsWithRef } from "react"
 import { View } from "react-native"
 import { twMerge } from "tailwind-merge"
-
+import {LoaderCircle} from "lucide-react-native"
 
 type LoadingProps =  {
 
 } & ComponentPropsWithRef<typeof View>
 
-export const Loading = ({ children, className, ...props } : LoadingProps) => {
-  return <View className={twMerge(className, "w-full h-full")}  {...props}>
-    { children }
+export const Loading = ({ className, ...props } : LoadingProps) => {
+  return <View className={twMerge(className, "w-full h-full flex items-center justify-center")}  {...props}>
+    <View className="animate-spin">
+      <LoaderCircle width={50} height={50}/>
+    </View>
   </View>
 
 }
