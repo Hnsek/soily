edac9941b9a083a5c43499f5d7f125a84db39722
Hnsek <henrique2001sousa@gmail.com>
2025-12-31 17:01:07 -0300

    refactor: isolates taximeter screen

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Taximeter } from './features/taximeter';
-
+import { TaximeterScreen } from './features/taximeter/TaximeterScreen';
 const Stack = createNativeStackNavigator()
 
 export default () => {
     return <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown:false}}>
-            <Stack.Screen name='taximeter' component={Taximeter}></Stack.Screen>
+            <Stack.Screen name='taximeter' component={TaximeterScreen}></Stack.Screen>
         </Stack.Navigator>
     </NavigationContainer>
 }
