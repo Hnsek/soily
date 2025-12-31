906d3cab9c6d780b99ac4bec0e9036ab4f4af07e
Hnsek <henrique2001sousa@gmail.com>
2025-12-31 16:26:22 -0300

    chore: creates location service

---
+++ src/services/location.ts
@@ -0,0 +1,24 @@
+import Geolocation, { GeolocationResponse } from "@react-native-community/geolocation";
+import { Position } from "../types/location";
+
+const format = (position : GeolocationResponse) : Position => {
+  return {
+    location: position.coords,
+    timestamp: position.timestamp
+  }
+}
+
+export const getCurrentPosition = () : Promise<Position> => {
+  return new Promise((resolve, reject) => {
+    Geolocation.getCurrentPosition(
+      (position) => resolve(format(position)),
+      (error) => reject(error)
+    )
+  })
+}
+
+export const watchPosition = (success: (position: Position) => void) => {
+  Geolocation.watchPosition(
+    (position) => success(format(position)),
+  )
+}
