3cf111e8928ee6f26f5c0a1a0cfa7c3270f4e090
Hnsek <henrique2001sousa@gmail.com>
2025-12-31 16:53:12 -0300

    feat: implements loading screen while searching initial location

--- src/features/taximeter/index.tsx
+++ src/features/taximeter/index.tsx
@@ -2,10 +2,34 @@ import { SafeAreaView } from "react-native-safe-area-context"
 import { Map, MapCamera } from "../../components/Map"
 import useLocation from "../../hooks/useLocation"
 import { useCamera } from "../../hooks/useCameraRef"
+import { Loading } from "../../components/Loading"
+import { useEffect, useState } from "react"
+import { getCurrentPosition } from "../../services/location"
+import { Location } from "../../types/location"
+
+export const TaximeterScreen = () => {
+  const [location, setLocation] = useState<Location>()
+
+  useEffect(() => {
+    getCurrentPosition()
+      .then((position) => setLocation(position.location))
+  }, [])
 
 
-export const Taximeter = () => {
-  const { location } = useLocation()  
+  if(!location){
+    return <Loading/>
+  }
+
+  return <Taximeter initialLocation = {location}/>
+}
+
+type TaximeterProps = {
+  initialLocation:  Location
+}
+
+export const Taximeter = ({ initialLocation} : TaximeterProps) => {
+  
+  const { location } = useLocation(initialLocation)  
 
   const { cameraRef } = useCamera(location)
 
