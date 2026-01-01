307c4b2eeb80b7ec04eb378ad5c62014918c8050
Hnsek <henrique2001sousa@gmail.com>
2026-01-01 18:25:53 -0300

    fix: fixes bottomsheet view in Taximeter

--- src/features/taximeter/Taximeter.tsx
+++ src/features/taximeter/Taximeter.tsx
@@ -5,10 +5,10 @@ import { useCamera } from "../../hooks/useCameraRef"
 import { Location } from "../../types/location"
 import { Car } from "lucide-react-native"
 import { BottomSheet, BottomSheetView } from "../../components/BottomSheet"
-import { Text } from "react-native"
+import { Text, View } from "react-native"
 import { TextInput } from "../../components/Input"
-import { useState } from "react"
 import { useTaximeterForm } from "./forms/useTaximeterForm"
+// import Picker from "react-native-picker-select"
 
 type TaximeterProps = {
   initialLocation:  Location
@@ -32,8 +32,21 @@ export const Taximeter = ({ initialLocation} : TaximeterProps) => {
       </Map>
       <BottomSheet className="absolute h-full w-full" enableDynamicSizing enableHandlePanningGesture enableContentPanningGesture>
         <BottomSheetView className="p-5 gap-2">
-          <Text className="text-xl font-bold">Value per KM</Text>
-          <TextInput {...form.register("value")} inputMode="numeric" keyboardType="numeric"/>
+        <View>
+            <Text className="text-xl font-bold">Currency</Text>
+            {/* <Picker */}
+            {/*   onValueChange={(value : string) => form.setValue("currency", value)} */}
+            {/*   items={[ */}
+            {/*     { label:"Dollar ($)", value:"dolar" }, */}
+            {/*     { label:"Euro (€)", value: "euro"}, */}
+            {/*     { label: "Real (R$)", value: "real"} */}
+            {/*   ]} */}
+            {/* /> */}
+          </View>
+          <View>
+            <Text className="text-xl font-bold">Value per KM</Text>
+            <TextInput {...form.register("value")} inputMode="numeric" keyboardType="numeric"/>
+          </View>
         </BottomSheetView>
       </BottomSheet>
 </SafeAreaView>
