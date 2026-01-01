d17bff44d92f99401e9ff06a29f1fa9ceb60e14c
Hnsek <henrique2001sousa@gmail.com>
2026-01-01 10:28:38 -0300

    feat: implements AnimatedMapMarker in Taximeter component

--- src/features/taximeter/Taximeter.tsx
+++ src/features/taximeter/Taximeter.tsx
@@ -1,23 +1,39 @@
 import { SafeAreaView } from "react-native-safe-area-context"
-import { Map, MapCamera } from "../../components/Map"
+import { AnimatedMapMarker, Map, MapCamera, MapMarker } from "../../components/Map"
 import useLocation from "../../hooks/useLocation"
 import { useCamera } from "../../hooks/useCameraRef"
 import { Location } from "../../types/location"
-
+import useAnimatedLocation from "../../hooks/useAnimatedLocation"
+import { Car } from "lucide-react-native"
 
 type TaximeterProps = {
   initialLocation:  Location
 }
 
+// const AnimatedMapMarker = Animated.createAnimatedComponent(MapMarker as ComponentType<{coordinate: number[], children: ReactElement}>)
+
 export const Taximeter = ({ initialLocation} : TaximeterProps) => {
   
-  const { location } = useLocation(initialLocation)  
+  const { location } = useLocation(initialLocation) as { location : Location}  
 
   const { cameraRef } = useCamera(location)
+  
+  const animatedLocation = useAnimatedLocation(location)
 
   return <SafeAreaView>
       <Map>
         <MapCamera ref={cameraRef}/>
+        <AnimatedMapMarker coordinate={[location.longitude, location.latitude]}>
+          <Car height={40} width={40}/>
+        </AnimatedMapMarker>
+
+        {/*   coordinate:[animatedLocation.longitude, animatedLocation.latitude]  */}
+              {/* }}>          */}
+              {/*   <Car height={40} width={40}/> */}
+              {/* </AnimatedMapMarker> */}
+        {/* <MapMarker coordinate={location ? [location.longitude, location.latitude] : []}> */}
+        {/*   <Car height={40} width={40}/> */}
+        {/* </MapMarker> */}
       </Map>
 </SafeAreaView>
 }
