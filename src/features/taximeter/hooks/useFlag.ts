e62071871cbd66b7350d6dea0e0fdf5b23d20705
Hnsek <henrique2001sousa@gmail.com>
2026-01-02 16:12:21 -0300

    chore: creates useFlag hook

---
+++ src/features/taximeter/hooks/useFlag.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+import { Flag } from "../types/Flag"
+import { Location } from "../../../types/location"
+
+const generateNewFlag = (oldFlag : Flag, location: Location) => {
+  const route = [...oldFlag.route, [location.longitude, location.latitude]]  
+  const flag = Object.assign(oldFlag, { route })
+  
+  return flag
+}
+
+const defaultFlag : Flag = {
+  currency:"dollar",
+  price: 0,
+  route: []
+}
+
+export const useFlag = (location?: Location) => {
+  const [ flag, setFlag ] = useState<Flag>(defaultFlag)
+  const [started, setStarted] = useState<boolean>(false)
+  
+  useEffect(() => {
+    if(!started || !location){
+      return
+    }
+  
+    setFlag(generateNewFlag(flag, location))
+
+  }, [location])  
+
+  const start = (flag : Flag ) => {
+    setStarted(true)
+    setFlag(flag)
+  }
+
+  const reset = () => setFlag(defaultFlag)
+  const stop = () => setStarted(false)
+
+  return {
+    start,
+    flag,
+    reset,
+    stop
+  }
+}
