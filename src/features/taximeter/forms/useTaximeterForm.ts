4b1e312b4f2e02e05a2fb4c5c173a31de1bb0f84
Hnsek <henrique2001sousa@gmail.com>
2026-01-02 11:19:15 -0300

    chore: changes value type from number to string in useTaximeterForm

--- src/features/taximeter/forms/useTaximeterForm.ts
+++ src/features/taximeter/forms/useTaximeterForm.ts
@@ -3,15 +3,16 @@ import { useForm } from "react-hook-form"
 import z from "zod"
 
 export const schema = z.object({
-  currency: z.string(),
-  value: z.number()
+  currency: z.string().nonempty(),
+  value: z.string().nonempty()
 })
 
 export const useTaximeterForm = () => {
   const form = useForm({
    resolver: zodResolver(schema),
    defaultValues:{
-     currency:"dollar"
+    currency:"dollar",
+    value: "0"
    }
   })
 
