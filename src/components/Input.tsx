0992ce86d882acba81ca7fccecc5bc6ca77a87bc
Hnsek <henrique2001sousa@gmail.com>
2026-01-02 10:35:22 -0300

    feat: adds custom styles in input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,5 +5,6 @@ import { twMerge } from "tailwind-merge";
 type TextInputProps = ComponentProps<typeof TextInputRoot>
 
 export const TextInput = ({ children, className, ...props } : TextInputProps) => {
-  return <TextInputRoot className={twMerge("h-16 w-full border rounded-md", className)} {...props}>{children}</TextInputRoot> 
+  return <TextInputRoot className={twMerge("h-16 w-full border rounded-md text-xl p-3", className)} {...props}>{children}</TextInputRoot> 
+  
 }
