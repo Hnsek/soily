2e5f96cd4838d307a8f06a7bb446713a9ac08ddd
Hnsek <henrique2001sousa@gmail.com>
2026-01-02 10:36:15 -0300

    feat: implments button to start

--- src/features/taximeter/Taximeter.tsx
+++ src/features/taximeter/Taximeter.tsx
@@ -8,7 +8,7 @@ import { BottomSheet, BottomSheetView } from "../../components/BottomSheet"
 import { Text, View } from "react-native"
 import { TextInput } from "../../components/Input"
 import { useTaximeterForm } from "./forms/useTaximeterForm"
-// import Picker from "react-native-picker-select"
+import { Button } from "../../components/Button"
 
 type TaximeterProps = {
   initialLocation:  Location
@@ -32,20 +32,18 @@ export const Taximeter = ({ initialLocation} : TaximeterProps) => {
       </Map>
       <BottomSheet className="absolute h-full w-full" enableDynamicSizing enableHandlePanningGesture enableContentPanningGesture>
         <BottomSheetView className="p-5 gap-2">
-        <View>
-            <Text className="text-xl font-bold">Currency</Text>
-            {/* <Picker */}
-            {/*   onValueChange={(value : string) => form.setValue("currency", value)} */}
-            {/*   items={[ */}
-            {/*     { label:"Dollar ($)", value:"dolar" }, */}
-            {/*     { label:"Euro (€)", value: "euro"}, */}
-            {/*     { label: "Real (R$)", value: "real"} */}
-            {/*   ]} */}
-            {/* /> */}
+          {/* <View> */}
+          {/*   <Text className="text-xl font-bold">Currency symbol</Text> */}
+          {/*   <TextInput {...form.register("currency")} defaultValue="$"/> */}
+          {/* </View> */}
+          <View>
+            <Text className="text-xl font-bold">Price per KM ($)</Text>
+            <TextInput {...form.register("value")} inputMode="numeric" keyboardType="numeric"/>
           </View>
           <View>
-            <Text className="text-xl font-bold">Value per KM</Text>
-            <TextInput {...form.register("value")} inputMode="numeric" keyboardType="numeric"/>
+            <Button>
+              <Text className="text-xl text-white font-bold">Start</Text>
+            </Button>
           </View>
         </BottomSheetView>
       </BottomSheet>
