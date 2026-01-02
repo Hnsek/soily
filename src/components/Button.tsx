ad61b539ed7d747bd16488e5703c1643558724d4
Hnsek <henrique2001sousa@gmail.com>
2026-01-02 11:18:49 -0300

    feat: adds custom styles in Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,5 +5,5 @@ import { twMerge } from "tailwind-merge";
 export type ButtonProps = ComponentProps<typeof Pressable>
 
 export const Button = ({children, className, ...props} : ButtonProps) => {
-  return <Pressable className={twMerge("bg-primary p-5 border rounded-md flex justify-center items-center flex-row", className)} {...props}>{children}</Pressable>
+  return <Pressable className={twMerge("bg-primary p-5 border rounded-md flex justify-center items-center flex-row active:opacity-60", className)} {...props}>{children}</Pressable>
 }
