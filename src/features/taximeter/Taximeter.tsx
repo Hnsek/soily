6f0210ffd25fa43e2af374df2bbc8cc10ed0a90f
Hnsek <henrique2001sousa@gmail.com>
2026-01-02 14:09:45 -0300

    refactor: isolates start taximeter in a new component

--- src/features/taximeter/Taximeter.tsx
+++ src/features/taximeter/Taximeter.tsx
@@ -11,6 +11,7 @@ import { useTaximeterForm } from "./forms/useTaximeterForm"
 import { Button } from "../../components/Button"
 import { Controller } from "react-hook-form"
 import { filterTextAsNumber } from "./utils/filterTextAsNumber"
+import { StartTaximeter } from "./components/StartTaximeter"
 
 type TaximeterProps = {
   initialLocation:  Location
@@ -32,32 +33,6 @@ export const Taximeter = ({ initialLocation} : TaximeterProps) => {
           <Car height={40} width={40}/>
         </AnimatedMapMarker>
       </Map>
-      <BottomSheet index={1} snapPoints={["5%","25%"]} className="absolute h-full w-full" enableDynamicSizing enableHandlePanningGesture enableContentPanningGesture>
-        <BottomSheetView className="p-5 gap-2">
-          <Controller
-            control={form.control}
-            name="value"
-            render={({field}) => {
-             return <View>
-              <Text className="text-xl font-bold">Price per KM ($)</Text>     
-                <TextInput onChangeText={(text) => field.onChange(filterTextAsNumber(text))} value={`${field.value}`} inputMode="numeric" keyboardType="numeric"/>
-              </View>                                                               
-            }}
-            />
-          
-          <View>
-            <Button onPress={()=> {
-                if(!form.formState.isValid){
-                   return
-                }
-
-                console.warn(form.getValues())
-
-            }}>
-              <Text className="text-xl text-white font-bold">Start</Text>
-            </Button>
-          </View>
-        </BottomSheetView>
-      </BottomSheet>
+      <StartTaximeter/>
 </SafeAreaView>
 }
