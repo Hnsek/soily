ab3f3bdacbe068530dc84ee897115cebb1ddf1b1
Hnsek <henrique2001sousa@gmail.com>
2026-01-04 18:07:00 -0300

    chore: adds missing methods in flag service with watermelonDB

--- src/services/flag.ts
+++ src/services/flag.ts
@@ -5,20 +5,44 @@ import { Flag } from "../features/taximeter/types/Flag"
 const table = database.get<FlagModel>("flag")
 
 export const createFlag = async (flag: Flag) : Promise<Flag>=> {
-  const {price, distance, currency, route, id} = await database.write(() => {
+  const model = await database.write(() => {
     return table.create((model) => {
       Object.assign(model, flag)
     })
   })
-  return {price, distance, currency, route, id} 
+  return extractProperties(model)
 }
 
-// export const updateFlag = (flag: Flag) => {
-//   table.get
-// }
+export const updateFlag = async (id: string, flag: Flag) => {
+  const model = await table.find(id);
+  
+  const updatedModel = await database.write(() => {
+    return model.update((model) => Object.assign(model, flag))
+  })
+
+  return extractProperties(updatedModel)
+}
 
 // export const deleteFlag = (flag: Flag) => database.remove(flag)
 
-export const getFlag = (id: string) => table.find(id)
-
+export const getFlag = async (id: string) => {
+  const model = await table.find(id)
+  return extractProperties(model)
+}
 // export const watchFlag = (options?: PouchDB.Core.ChangesOptions) => database.changes(options)
+//
+
+export const upsertFlag = async (id: string, flag: Flag) => {
+  const model = await table.find(id)
+
+  if(model.id){
+      return model.update((model) => Object.assign(model,flag))
+  }
+
+  return createFlag(flag)
+}
+
+const extractProperties = (model : FlagModel) => {
+  const {price, distance, currency, route, id} = model
+  return {price, distance, currency, route, id}  
+}
