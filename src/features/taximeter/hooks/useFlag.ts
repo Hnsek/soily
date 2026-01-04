e3d9e4b46a8681e9c468b1205af6866dce6db3a9
Hnsek <henrique2001sousa@gmail.com>
2026-01-04 09:33:38 -0300

    feat: implements distance calculation in useFlag

--- src/features/taximeter/hooks/useFlag.ts
+++ src/features/taximeter/hooks/useFlag.ts
@@ -1,32 +1,37 @@
 import { useEffect, useState } from "react"
 import { Flag } from "../types/Flag"
 import { Location } from "../../../types/location"
+import { getPathLength } from "../../../services/geospatial"
+import { convertCoordinatesArrayToObject } from "../utils/convert-coordinates"
 
 const generateNewFlag = (oldFlag : Flag, location: Location) => {
-  const route = [...oldFlag.route, [location.longitude, location.latitude]]  
-  const flag = Object.assign(oldFlag, { route })
-  
-  return flag
+  const route : [longitude : number, latitude : number][] = [...oldFlag.route, [location.longitude, location.latitude]]  
+  const distance = getPathLength(convertCoordinatesArrayToObject(route))
+
+  return Object.assign(oldFlag, { route, distance })
+    
 }
 
 const defaultFlag : Flag = {
   currency:"dollar",
   price: 0,
-  route: []
+  route: [],
+  distance: 0
 }
 
 export const useFlag = (location?: Location) => {
   const [ flag, setFlag ] = useState<Flag>(defaultFlag)
   const [started, setStarted] = useState<boolean>(false)
-  
+
   useEffect(() => {
+
     if(!started || !location){
       return
     }
   
     setFlag(generateNewFlag(flag, location))
 
-  }, [location])  
+  }, [location?.latitude, location?.longitude, started])  
 
   const start = (flag : Flag ) => {
     setStarted(true)
