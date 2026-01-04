900af04718e0a25b8b3e4d740d508d1525ebddf7
Hnsek <henrique2001sousa@gmail.com>
2026-01-04 09:32:09 -0300

    feat: implements flag information in running taximeter

--- src/features/taximeter/components/RunningTaximeter.tsx
+++ src/features/taximeter/components/RunningTaximeter.tsx
@@ -1,35 +1,40 @@
 import { BottomSheet, BottomSheetView } from "../../../components/BottomSheet"
 import { Text, View } from "react-native"
-import { TextInput } from "../../../components/Input"
 import { Button } from "../../../components/Button"
-import { Controller } from "react-hook-form"
-import { filterTextAsNumber } from "../utils/filterTextAsNumber"
-import { useTaximeterForm } from "../hooks/useTaximeterForm"
+import { Flag } from "../types/Flag"
 
+type RunningTaximeterProps = {
+  flag:Flag
+}
 
-export const RunningTaximeter = () => {
-  const form = useTaximeterForm()
+const calculateFinalPrice = (distance : number, price: number) => {
+  if(!distance) {
+    return 0
+  }
+
+  return (distance / 1000) * price
+} 
+
+export const RunningTaximeter = ({ flag }: RunningTaximeterProps) => {
   
+  
+
   return <BottomSheet index={1} snapPoints={["5%","25%"]} className="absolute h-full w-full" enableDynamicSizing enableHandlePanningGesture enableContentPanningGesture>
     <BottomSheetView className="p-5 gap-2">
-      <Controller
-        control={form.control}
-        name="price"
-        render={({field}) => {
-         return <View>
-          <Text className="text-xl font-bold">Price per KM ($)</Text>     
-            <TextInput onChangeText={(text) => field.onChange(filterTextAsNumber(text))} value={`${field.value}`} inputMode="numeric" keyboardType="numeric"/>
-            </View>                                                               
-          }}
-          />          
+      <View className="flex flex-row">
+        <View className="flex-1">
+          <Text className="text-xl font-bold">Price per KM: ${flag.price}</Text>     
+          <Text className="text-xl font-bold">Distance: {flag.distance ? flag.distance / 1000 : 0} KM</Text>     
+        </View>                      
+
+        <View className="flex-1 flex flex-row items-center justify-end">
+          <Text className="text-3xl font-bold">${calculateFinalPrice(flag.distance, flag.price)}</Text>     
+        </View>
+      </View>                                                               
       <View>
-        <Button onPress={()=> {
-          if(!form.formState.isValid){
-             return
-          }
-             console.warn(form.getValues())
+        <Button className="bg-red-500" onPress={()=> {
           }}>
-            <Text className="text-xl text-white font-bold">Start</Text>
+            <Text className="text-xl text-white font-bold">Finish</Text>
         </Button>
       </View>        
     </BottomSheetView>
