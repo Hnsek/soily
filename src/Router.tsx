84c5c968f1abba71434cff91def3dc0a45563135
Hnsek <henrique2001sousa@gmail.com>
2026-01-05 10:16:33 -0300

    chore: adds TaximeterDetails to the router list

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { TaximeterScreen } from './features/taximeter/TaximeterScreen';
+import { TaximeterDetails } from './features/taximeter/TaximeterDetails';
 const Stack = createNativeStackNavigator()
 
 export default () => {
     return <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown:false}}>
-            <Stack.Screen name='taximeter' component={TaximeterScreen}></Stack.Screen>
+            <Stack.Screen name='Taximeter' component={TaximeterScreen}></Stack.Screen>
+            <Stack.Screen name='TaximeterDetails' component={TaximeterDetails}></Stack.Screen>
         </Stack.Navigator>
     </NavigationContainer>
 }
