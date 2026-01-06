60e92f07c9f521c096b96c9315babc846b05f019
Hnsek <henrique2001sousa@gmail.com>
2026-01-06 17:52:54 -0300

    chore: implements getData method of flag model

--- src/services/flag.ts
+++ src/services/flag.ts
@@ -1,31 +1,34 @@
 import { database } from "../database"
 import { FlagModel } from "../database/models/flag"
+import { CreateFlag } from "../features/taximeter/dtos/CreateFlag"
+import { UpdateFlag } from "../features/taximeter/dtos/UpdateFlag"
 import { Flag } from "../features/taximeter/types/Flag"
 
 const table = database.get<FlagModel>("flag")
 
-export const createFlag = async (flag: Flag) : Promise<Flag>=> {
+export const createFlag = async (flag: CreateFlag) : Promise<Flag>=> {
   const model = await database.write(() => {
     return table.create((model) => {
-      Object.assign(model, flag)
+      assignFlagToModel(model, flag)
     })
   })
-  return model
+
+  return model.getData()
 }
 
 export const updateFlag = async (id: string, flag: Flag) => {
   const model = await table.find(id);
   
   const updatedModel = await database.write(() => {
-    return model.update((model) => Object.assign(model, flag))
+    return model.update((model) => assignFlagToModel(model, flag))
   })
 
-  return updatedModel
+  return updatedModel.getData()
 }
 
 export const getFlag = async (id: string) => {
   const model = await table.find(id)
-  return model
+  return model.getData()
 }
 
 export const upsertFlag = async (flag: Flag) => {
@@ -33,11 +36,18 @@ export const upsertFlag = async (flag: Flag) => {
     return createFlag(flag)
   }
 
-  const model = await table.find(flag.id)
+  const model = await getFlag(flag.id)
 
   if(model.id){
-      return model.update((model) => Object.assign(model,flag))
+    return updateFlag(flag.id, flag)
   }
 
   return createFlag(flag)
+}
+
+const assignFlagToModel = (model : FlagModel, flag : CreateFlag | UpdateFlag) => {
+  model.price = flag.price
+  model.currency = flag.currency
+  model.distance = flag.distance
+  model.route = flag.route
 }
