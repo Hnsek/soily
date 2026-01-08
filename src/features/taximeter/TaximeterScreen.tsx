5f870e4058003b63d24b29c0fdb27459221d78e7
Hnsek <henrique2001sousa@gmail.com>
2026-01-08 18:56:16 -0300

    fix: adds permission requesting in TaximeterScreen

--- src/features/taximeter/TaximeterScreen.tsx
+++ src/features/taximeter/TaximeterScreen.tsx
@@ -3,13 +3,28 @@ import { getCurrentPosition } from "../../services/location"
 import { Location } from "../../types/location"
 import { Loading } from "../../components/Loading"
 import { Taximeter } from "./Taximeter"
+import { useLocationPermissions } from "@gabriel-sisjr/react-native-background-location"
 
 export const TaximeterScreen = () => {
   const [location, setLocation] = useState<Location>()
+  
+  const { permissionStatus, requestPermissions } = useLocationPermissions();
 
   useEffect(() => {
-    getCurrentPosition()
-      .then((position) => setLocation(position.location))
+    
+    const init = async () => {
+      if(!permissionStatus.hasPermission){
+        const result = await requestPermissions()
+        
+        if(!result) {}
+      }
+
+      const position = await getCurrentPosition()
+    
+      setLocation(position.location)
+    }
+
+    init()
   }, [])
 
 
