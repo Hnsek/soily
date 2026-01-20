3389be53bf9d671e42bc482f7e396d847ecec9dc
Hnsek <henrique2001sousa@gmail.com>
2026-01-20 11:34:43 -0300

    feat: implements navigation to CreatePlanting in ListPlanting

--- src/features/planting/ListPlanting.tsx
+++ src/features/planting/ListPlanting.tsx
@@ -1,11 +1,16 @@
 import { Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Button } from "../../components/Button"
+import { RouteList } from "../../Router"
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-export const ListPlanting = () => {
+type Props = NativeStackScreenProps<RouteList,"ListPlanting">
+export const ListPlanting = ({navigation} : Props) => {
+
+
   return <SafeAreaView className="h-screen w-screen bg-background">
     <View className="w-full absolute bottom-0 p-4 flex items-center justify-center">
-      <Button className="w-full">
+      <Button className="w-full" onPressOut={() => navigation.navigate("CreatePlanting")}>
         <Text className="text-white text-2xl">Registrar</Text>
       </Button> 
     </View> 
