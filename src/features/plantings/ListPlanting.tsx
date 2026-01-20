81cd021de8f47ee3e9537b72e95fd111c837a7f1
Hnsek <henrique2001sousa@gmail.com>
2026-01-20 18:12:30 -0300

    refactor: implement composite screen props in ListPlanting

--- src/features/plantings/ListPlanting.tsx
+++ src/features/plantings/ListPlanting.tsx
@@ -1,14 +1,19 @@
 import { Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Button } from "../../components/Button"
-import { BottomTabRouteList } from "../../Router"
+import { BottomTabRouteList, StackRouteList } from "../../Router"
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { CompositeScreenProps } from "@react-navigation/native";
+import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 
-type Props = NativeStackScreenProps<BottomTabRouteList,"ListPlanting">
+type Props = CompositeScreenProps<
+  BottomTabScreenProps<BottomTabRouteList,"ListPlanting">,
+  NativeStackScreenProps<StackRouteList,"CreatePlanting">
+>
 export const ListPlanting = ({navigation} : Props) => {
   return <SafeAreaView className="h-screen w-screen bg-background">
     <View className="w-full absolute bottom-0 p-4 flex items-center justify-center">
-      <Button className="w-full" onPressOut={() => navigation.getParent()?.navigate("CreatePlanting")}>
+      <Button className="w-full" onPressOut={() => navigation.navigate("CreatePlanting")}>
         <Text className="text-white text-2xl">Registrar</Text>
       </Button> 
     </View> 
