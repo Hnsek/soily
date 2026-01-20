dad05e7ca4b7200054bbbc46f008293e2702bea0
Hnsek <henrique2001sousa@gmail.com>
2026-01-20 11:16:37 -0300

    feat: changes color of the register button text in ListPlanting

--- src/features/planting/ListPlanting.tsx
+++ src/features/planting/ListPlanting.tsx
@@ -6,7 +6,7 @@ export const ListPlanting = () => {
   return <SafeAreaView className="h-screen w-screen bg-background">
     <View className="w-full absolute bottom-0 p-4 flex items-center justify-center">
       <Button className="w-full">
-        <Text>Registrar</Text>
+        <Text className="text-white text-2xl">Registrar</Text>
       </Button> 
     </View> 
   </SafeAreaView>
