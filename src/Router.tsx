f29c2eff45f47b940919afa0503efec065b62280
Hnsek <henrique2001sousa@gmail.com>
2026-01-20 10:13:25 -0300

    chore: removes Easyroute routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { TaximeterScreen } from './features/taximeter/TaximeterScreen';
-import { TaximeterDetails } from './features/taximeter/TaximeterDetails';
 import { Flag } from './features/taximeter/types/Flag';
 import { PlantingList } from './features/planting/PlantingList';
 
@@ -16,8 +14,6 @@ const Stack = createNativeStackNavigator<RouteList>()
 export default () => {
     return <NavigationContainer>
         <Stack.Navigator initialRouteName='PlantingList' screenOptions={{headerShown:false}}>
-            <Stack.Screen name='Taximeter' component={TaximeterScreen}></Stack.Screen>
-            <Stack.Screen name='TaximeterDetails' component={TaximeterDetails}></Stack.Screen>
             <Stack.Screen name="PlantingList" component={PlantingList}></Stack.Screen>
         </Stack.Navigator>
     </NavigationContainer>
