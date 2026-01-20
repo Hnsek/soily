b998777ed96308c7ae4967a6dabfaf9a5db9dedf
Hnsek <henrique2001sousa@gmail.com>
2026-01-20 17:32:57 -0300

    fix: change to use parent navigation in ListPlanting

--- src/features/plantings/ListPlanting.tsx
+++ src/features/plantings/ListPlanting.tsx
@@ -10,7 +10,7 @@ export const ListPlanting = ({navigation} : Props) => {
 
   return <SafeAreaView className="h-screen w-screen bg-background">
     <View className="w-full absolute bottom-0 p-4 flex items-center justify-center">
-      <Button className="w-full" onPressOut={() => navigation.navigate("CreatePlanting")}>
+      <Button className="w-full" onPressOut={() => navigation.getParent()?.navigate("CreatePlanting")}>
         <Text className="text-white text-2xl">Registrar</Text>
       </Button> 
     </View> 
