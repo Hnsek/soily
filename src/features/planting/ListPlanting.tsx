f456450a567c7eb132f64297f216dc1598ca7149
Hnsek <henrique2001sousa@gmail.com>
2026-01-20 10:54:40 -0300

    chore: adds background-color in container in ListPlanting

--- src/features/planting/ListPlanting.tsx
+++ src/features/planting/ListPlanting.tsx
@@ -1,5 +1,7 @@
 import { SafeAreaView } from "react-native-safe-area-context"
 
 export const PlantingList = () => {
-  return <SafeAreaView className="h-screen w-screen"></SafeAreaView>
+  return <SafeAreaView className="h-screen w-screen bg-background">
+    
+  </SafeAreaView>
 }
