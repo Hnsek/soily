5feecbc606cc75ad380b835ef186ab785c8f1fea
Hnsek <henrique2001sousa@gmail.com>
2026-01-20 17:12:27 -0300

    refactor: implements bottom bar with stack navigation in Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { ListPlanting } from './features/plantings/ListPlanting';
 import { CreatePlanting } from './features/plantings/CreatePlanting';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export type BottomTabRouteList = {
   ListPlanting: undefined;
@@ -10,11 +11,28 @@ export type BottomTabRouteList = {
 
 const BottomTab = createBottomTabNavigator<BottomTabRouteList>()
 
+const BottomTabScreens =() => {
+  return <BottomTab.Navigator initialRouteName='ListPlanting' screenOptions={{headerShown:false}}>
+      <BottomTab.Screen name="ListPlanting" component={ListPlanting}></BottomTab.Screen>
+    </BottomTab.Navigator>    
+}
+
+export type StackRouteList = {
+  CreatePlanting: undefined;
+  Tabs: undefined
+}
+
+const Stack = createNativeStackNavigator<StackRouteList>()
+
+const StackScreens = () => {
+   return <Stack.Navigator initialRouteName='Tabs' screenOptions={{headerShown: false}}>
+      <Stack.Screen name='Tabs' component={BottomTabScreens}></Stack.Screen>
+      <Stack.Screen name='CreatePlanting' component={CreatePlanting}></Stack.Screen>
+    </Stack.Navigator>
+}
+
 export default () => {
   return <NavigationContainer>
-    <BottomTab.Navigator initialRouteName='ListPlanting' screenOptions={{headerShown:false}}>
-      <BottomTab.Screen name="ListPlanting" component={ListPlanting}></BottomTab.Screen>
-      <BottomTab.Screen name="CreatePlanting" component={CreatePlanting} options={{ tabBarStyle:{display: "none"} }}></BottomTab.Screen>
-    </BottomTab.Navigator>    
+    <StackScreens/>
   </NavigationContainer>
 }
