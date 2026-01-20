036de6a6530b325f3558f752efc08781368b8497
Hnsek <henrique2001sousa@gmail.com>
2026-01-20 11:18:04 -0300

    chore: adds CreatePlanting route in Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ListPlanting } from './features/planting/ListPlanting';
+import { CreatePlanting } from './features/planting/CreatePlanting';
 
 export type RouteList = {
   ListPlanting: undefined;
+  CreatePlanting: undefined;
 }
 
 const Stack = createNativeStackNavigator<RouteList>()
@@ -12,6 +14,7 @@ export default () => {
     return <NavigationContainer>
         <Stack.Navigator initialRouteName='ListPlanting' screenOptions={{headerShown:false}}>
             <Stack.Screen name="ListPlanting" component={ListPlanting}></Stack.Screen>
+            <Stack.Screen name="CreatePlanting" component={CreatePlanting}></Stack.Screen>
         </Stack.Navigator>
     </NavigationContainer>
 }
