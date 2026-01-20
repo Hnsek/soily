3b068ed82c764ba01f8d477d2a31df54d492828e
Hnsek <henrique2001sousa@gmail.com>
2026-01-20 17:37:36 -0300

    chore: implement register button in ListPlantMaterial

--- src/features/plant-material/ListPlantMaterial.tsx
+++ src/features/plant-material/ListPlantMaterial.tsx
@@ -1,5 +1,16 @@
+import { Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { Button } from "../../components/Button"
+import { BottomTabRouteList } from "../../Router"
+import { BottomTabScreenProps } from "@react-navigation/bottom-tabs"
+type Props = BottomTabScreenProps<BottomTabRouteList,"ListPlanting">
 
-export const ListPlantMaterial= ()=> {
-  return <SafeAreaView className="w-full h-full bg-background"></SafeAreaView>
+export const ListPlantMaterial= ({ navigation } : Props)=> {
+  return <SafeAreaView className="w-full h-full bg-background">
+    <View className="w-full absolute bottom-0 p-4 flex items-center justify-center">
+      <Button className="w-full" >
+        <Text className="text-white text-xl">Registrar</Text>
+      </Button> 
+    </View> 
+  </SafeAreaView>
 }
--- src/features/plantings/ListPlanting.tsx
+++ src/features/plantings/ListPlanting.tsx
@@ -6,8 +6,6 @@ import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 type Props = NativeStackScreenProps<BottomTabRouteList,"ListPlanting">
 export const ListPlanting = ({navigation} : Props) => {
-
-
   return <SafeAreaView className="h-screen w-screen bg-background">
     <View className="w-full absolute bottom-0 p-4 flex items-center justify-center">
       <Button className="w-full" onPressOut={() => navigation.getParent()?.navigate("CreatePlanting")}>
