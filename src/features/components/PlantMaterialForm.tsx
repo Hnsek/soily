fee1c82ded8a5cf4dc88c5ba912a72e32d0b766a
Hnsek <henrique2001sousa@gmail.com>
2026-01-22 18:09:23 -0300

    feat: create initial PlantMaterialForm (WIP)

---
+++ src/features/components/PlantMaterialForm.tsx
@@ -0,0 +1,8 @@
+import { View } from "react-native"
+import { Text } from "../../components/Text"
+
+export const PlantMaterialForm = () => {
+  return <View>
+    <Text>Criar semente/muda</Text>      
+  </View>
+}
