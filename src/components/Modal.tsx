e92d3d3bfe715b804ae22647334084bb7abbef81
Hnsek <henrique2001sousa@gmail.com>
2026-01-22 17:16:46 -0300

    chore: create modal context in Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,14 @@
+import { createContext } from "react"
 import { Modal } from "react-native"
+
+type ModalContextType = {
+  visible: boolean
+  toggle: () => unknown
+}
+const ModalContext = createContext<ModalContextType>({
+  visible: false,
+  toggle: () => undefined
+})
 
 export const ModalContainer = () => {
   return <Modal></Modal>
