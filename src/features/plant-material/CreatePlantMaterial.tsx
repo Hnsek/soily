cab42d393320e139a933235532f293848faefdae
Hnsek <henrique2001sousa@gmail.com>
2026-01-22 16:56:40 -0300

    chore: create Container component in CreatePlantMaterial

--- src/features/plant-material/CreatePlantMaterial.tsx
+++ src/features/plant-material/CreatePlantMaterial.tsx
@@ -1,5 +1,13 @@
+import { ComponentProps } from "react"
 import { View } from "react-native"
+import { twMerge } from "tailwind-merge"
 
 export const CreatePlantMaterial = () => {
-  return <View></View>
+  return <View className="h-full w-full">
+
+  </View>
+}
+
+const Container = ({children, className, ...props} : ComponentProps<typeof View>) => {
+  return <View className={twMerge("h-full w-full", className)} {...props}>{children}</View>
 }
