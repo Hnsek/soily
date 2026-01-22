38e599d81ffedcaaea3159a7734dec9e4428dc4d
Hnsek <henrique2001sousa@gmail.com>
2026-01-22 16:46:39 -0300

    chore: implement navigate to CreatePlantMaterial in ListPlantMaterial

--- src/features/plant-material/ListPlantMaterial.tsx
+++ src/features/plant-material/ListPlantMaterial.tsx
@@ -1,14 +1,20 @@
 import { Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Button } from "../../components/Button"
-import { BottomTabRouteList } from "../../Router"
+import { BottomTabRouteList, StackRouteList } from "../../Router"
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs"
-type Props = BottomTabScreenProps<BottomTabRouteList,"ListPlantMaterial">
+import { NativeStackScreenProps } from "@react-navigation/native-stack"
+import { CompositeScreenProps } from "@react-navigation/native"
+
+type Props = CompositeScreenProps<
+  BottomTabScreenProps<BottomTabRouteList,"ListPlantMaterial">,
+  NativeStackScreenProps<StackRouteList,"CreatePlantMaterial">
+>
 
 export const ListPlantMaterial= ({ navigation } : Props)=> {
   return <SafeAreaView className="w-full h-full bg-background">
     <View className="w-full absolute bottom-0 p-4 flex items-center justify-center">
-      <Button className="w-full" >
+      <Button className="w-full" onPressOut={() => navigation.navigate("CreatePlantMaterial")}>
         <Text className="text-white text-xl">Registrar</Text>
       </Button> 
     </View> 
