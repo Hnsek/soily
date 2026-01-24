bd0dd2c546c247cc0b31f2d64a671d079fa33b4d
Hnsek <henrique2001sousa@gmail.com>
2026-01-24 10:27:10 -0300

    feat: create Text component

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,6 +1,9 @@
-import { ComponentProps } from "react"
-import { Text as TextRoot } from "react-native"
+import { ComponentProps } from "react";
+import { Text as TextRoot} from "react-native"
+import { twMerge } from "tailwind-merge";
 
-export const Text = ({children, ...props} : ComponentProps<typeof TextRoot>) => {
-  return <Text {...props}>{children}</Text>
+export type TextProps = ComponentProps<typeof TextRoot>
+
+export const Text = ({children, className, ...props} : TextProps) => {
+  return <TextRoot className={twMerge("text-xl text-white", className)} {...props}>{children}</TextRoot>
 }
