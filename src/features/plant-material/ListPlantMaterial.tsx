e5cf2d1ee70e9bfded67494a52937f41e23e7606
Hnsek <henrique2001sousa@gmail.com>
2026-01-24 10:28:22 -0300

    feat: implement form modal in ListPlantMaterial

--- src/features/plant-material/ListPlantMaterial.tsx
+++ src/features/plant-material/ListPlantMaterial.tsx
@@ -1,22 +1,21 @@
-import { Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
-import { Button } from "../../components/Button"
-import { BottomTabRouteList, StackRouteList } from "../../Router"
+import { BottomTabRouteList } from "../../Router"
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs"
-import { NativeStackScreenProps } from "@react-navigation/native-stack"
-import { CompositeScreenProps } from "@react-navigation/native"
+import { Modal, ModalBackground, ModalContainer } from "../../components/Modal"
+import { PlantMaterialForm } from "./components/PlantMaterialForm"
+import { RegisterPlantMaterialButton } from "./components/RegisterPlantMaterialButton"
 
-type Props = CompositeScreenProps<
-  BottomTabScreenProps<BottomTabRouteList,"ListPlantMaterial">,
-  NativeStackScreenProps<StackRouteList,"CreatePlantMaterial">
->
+type Props = BottomTabScreenProps<BottomTabRouteList,"ListPlantMaterial">
 
 export const ListPlantMaterial= ({ navigation } : Props)=> {
   return <SafeAreaView className="w-full h-full bg-background">
-    <View className="w-full absolute bottom-0 p-4 flex items-center justify-center">
-      <Button className="w-full" onPressOut={() => navigation.navigate("CreatePlantMaterial")}>
-        <Text className="text-white text-xl">Registrar</Text>
-      </Button> 
-    </View> 
+    <ModalContainer>
+      <RegisterPlantMaterialButton/>
+      <Modal transparent>
+        <ModalBackground className="flex flex-col justify-end">
+          <PlantMaterialForm/>
+        </ModalBackground>  
+      </Modal>
+    </ModalContainer>
   </SafeAreaView>
 }
