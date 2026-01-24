5613b08a92e3df2bcb562a5480a222781e4df2c5
Hnsek <henrique2001sousa@gmail.com>
2026-01-24 10:27:42 -0300

    feat: create PlantMaterialForm component

---
+++ src/features/plant-material/components/PlantMaterialForm.tsx
@@ -0,0 +1,53 @@
+import { View } from "react-native"
+import { Button } from "../../../components/Button"
+import { z } from "zod"
+import { Controller, useForm } from "react-hook-form" 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { NumericInput, TextInput } from "../../../components/Input"
+import { Text } from "../../../components/Text"
+
+
+const schema = z.object({
+  name: z.string().nonempty(),
+  quantity: z.number().nonnegative()
+})
+
+type PlantMaterialFormType = z.output<typeof schema>
+
+type PlantMaterialFormProps = {
+  onSubmit?:(data : PlantMaterialFormType) => unknown
+}
+export const PlantMaterialForm = ({ onSubmit } : PlantMaterialFormProps) => {
+    
+  const { control, handleSubmit } = useForm({
+   resolver:  zodResolver(schema)
+  })
+  
+  const submit = (data : PlantMaterialFormType) => {
+    data.name = data.name.trim()
+
+    onSubmit?.(data)
+  }
+
+  return <View className="p-4 bg-background gap-4">
+      <Controller
+        name="name"
+        control={control}
+        render={({field})=>{
+          return <TextInput placeholder="Nome" {...field} onChangeText={field.onChange}/>
+        }}
+        />
+      
+      <Controller
+        name="quantity"
+        control={control}
+        render={({field})=>{
+          return <NumericInput placeholder="Quantidade" {...field} onChangeValue={field.onChange} delimiter=","/>
+        }}
+        />
+
+      <Button onPressOut={handleSubmit(submit)}>
+        <Text>Registrar</Text>
+      </Button>
+    </View>
+}
