a01829a8014b2531c45005a88a47357d57908b70
Hnsek <henrique2001sousa@gmail.com>
2026-01-24 10:26:40 -0300

    feat: create NumericInput

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,18 @@
 import { ComponentProps } from "react";
 import { TextInput as TextInputRoot } from "react-native"
 import { twMerge } from "tailwind-merge";
+import CurrencyInput from "react-native-currency-input"
 
 type TextInputProps = ComponentProps<typeof TextInputRoot>
 
 export const TextInput = ({ children, className, ...props } : TextInputProps) => {
   return <TextInputRoot className={twMerge("h-16 w-full border rounded-md text-xl p-3", className)} {...props}>{children}</TextInputRoot> 
   
+}
+
+type NumericInputProps = ComponentProps<typeof CurrencyInput>
+
+export const NumericInput = ({ children, className, ...props } : NumericInputProps) => {
+  return <CurrencyInput
+    className={twMerge("h-16 w-full border rounded-md text-xl p-3", className)} {...props}>{children}</CurrencyInput> 
 }
