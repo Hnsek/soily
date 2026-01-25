d88287981ca901f76efd64399693ad504c074138
Hnsek <henrique2001sousa@gmail.com>
2026-01-25 10:18:39 -0300

    chore: create modal store in Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps, createContext, ReactNode, useContext, useState } from "react"
+import { ComponentProps, createContext, ReactNode, useContext, useEffect, useState } from "react"
 import { Modal as ModalRoot, View} from "react-native"
 import { twMerge } from "tailwind-merge"
 
@@ -37,4 +37,33 @@ export const Modal = ({children, ...props} : ModalProps) => {
 type ModalBackgroundProps = ComponentProps<typeof View>
 export const ModalBackground = ({ children, className, ...props } : ModalBackgroundProps) => {
   return <View className={twMerge("h-full w-full bg-black/70", className)} {...props}>{children}</View> 
+}
+
+type ModalListener = {
+      visible:boolean,
+      setVisible: React.Dispatch<React.SetStateAction<boolean>>
+  }
+
+
+type ModalStore = {
+  listeners:{
+    [key: string] : ModalListener
+  },
+  show(id:string) : unknown,
+  close(id:string) : unknown,
+  subscribe(id : string, listener:ModalListener):unknown
+}
+
+export const modalStore : ModalStore= {
+  listeners:{},
+  show(id: string){
+    console.warn(this.listeners[id])
+    this.listeners[id].setVisible(true)
+  },
+  close(id:string){
+    this.listeners[id].setVisible(false)
+  },
+  subscribe(id : string, listener: ModalListener){
+    this.listeners[id] = listener
+  }
 }
