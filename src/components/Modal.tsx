a1ecc91fcafd3f3f6912e507b2436aaf6777bf57
Hnsek <henrique2001sousa@gmail.com>
2026-01-25 10:40:18 -0300

    chore: remove modal container

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,32 +2,6 @@ import { ComponentProps, createContext, ReactNode, useContext, useEffect, useSta
 import { Modal as ModalRoot, View} from "react-native"
 import { twMerge } from "tailwind-merge"
 
-type ModelContextType = {
-  visible: boolean,
-  show: () => unknown,
-  close: () => unknown
-}
-const ModalContext = createContext<ModelContextType>({
-  visible: false,
-  show: () => undefined,
-  close: () => undefined
-})
-
-export const useModal = () => useContext(ModalContext)
-
-type ModalContainerProps = {
-  children: ReactNode
-}
-
-export const ModalContainer = ({ children } : ModalContainerProps) => {
-  const [visible, setVisible] = useState(false)
-
-  const show = () => setVisible(true)
-  const close = () => setVisible(false)
-  
-  return <ModalContext.Provider value={{ visible, show, close}}>{children}</ModalContext.Provider>
-}
-
 type ModalProps = ComponentProps<typeof ModalRoot>
 export const Modal = ({children, ...props} : ModalProps) => {
 
--- src/features/plant-material/ListPlantMaterial.tsx
+++ src/features/plant-material/ListPlantMaterial.tsx
@@ -11,17 +11,15 @@ type Props = BottomTabScreenProps<BottomTabRouteList,"ListPlantMaterial">
 
 export const ListPlantMaterial= ({ navigation } : Props)=> {
   return <SafeAreaView className="w-full h-full bg-background">
-    <ModalContainer>
-      <Modal id="register-plant-material-modal" transparent>
-        <ModalBackground className="flex flex-col justify-end">
-          <PlantMaterialForm onSubmit={() => modalStore.close("register-plant-material-modal")}/>
-        </ModalBackground>  
-      </Modal>
-      <View className="w-full absolute bottom-0 p-4 flex items-center justify-center">
-        <Button className="w-full" onPressOut={() => modalStore.show("register-plant-material-modal")}>
-          <Text className="text-white text-xl">Registrar</Text>
-        </Button> 
-      </View>
-    </ModalContainer>
+    <Modal id="register-plant-material-modal" transparent>
+      <ModalBackground className="flex flex-col justify-end">
+        <PlantMaterialForm onSubmit={() => modalStore.close("register-plant-material-modal")}/>
+      </ModalBackground>  
+    </Modal>
+    <View className="w-full absolute bottom-0 p-4 flex items-center justify-center">
+      <Button className="w-full" onPressOut={() => modalStore.show("register-plant-material-modal")}>
+        <Text className="text-white text-xl">Registrar</Text>
+      </Button> 
+    </View>
   </SafeAreaView>
 }
