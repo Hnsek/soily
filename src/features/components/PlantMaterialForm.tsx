d5cb450f746697587d5ff62453abe89149288deb
Hnsek <henrique2001sousa@gmail.com>
2026-01-26 15:43:26 -0300

    chore: remove components folder in features

--- src/features/components/PlantMaterialForm.tsx
+++
@@ -1,8 +0,0 @@
-import { View } from "react-native"
-import { Text } from "../../components/Text"
-
-export const PlantMaterialForm = () => {
-  return <View>
-    <Text>Criar semente/muda</Text>      
-  </View>
-}
