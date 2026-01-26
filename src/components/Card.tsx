b30e15e738c043dfb1dd6a59f7589d16255001a6
Hnsek <henrique2001sousa@gmail.com>
2026-01-26 17:01:18 -0300

    feat: create CardText component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,13 @@
 import { ComponentProps } from "react"
-import { View } from "react-native"
+import { Text, View } from "react-native"
 import { twMerge } from "tailwind-merge"
 
 export type CardContainerProps = ComponentProps<typeof View>
 export const CardContainer = ({ children, className, ...props} : CardContainerProps) => {
-  return <View className={twMerge("", className)} {...props}>{children}</View>
+  return <View className={twMerge("rounded-md border w-full h-10", className)} {...props}>{children}</View>
+}
+
+type CardTextProps = ComponentProps<typeof Text>
+export const CardText = ({ children, ...props} : CardTextProps) => {
+  return <Text {...props}>{ children }</Text>
 }
