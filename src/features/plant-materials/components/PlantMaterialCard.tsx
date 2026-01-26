f9120d5136abbe3d26eeb33969857b638746cfdc
Hnsek <henrique2001sousa@gmail.com>
2026-01-26 15:52:23 -0300

    chore: add initial configuration to PlantMaterialCard props

--- src/features/plant-materials/components/PlantMaterialCard.tsx
+++ src/features/plant-materials/components/PlantMaterialCard.tsx
@@ -1,5 +1,11 @@
+import { ComponentProps } from "react"
 import { View } from "react-native"
+import { twMerge } from "tailwind-merge"
 
-export const PlantMaterialCard = () => {
-  return <View></View>
+type PlantMaterialCardProps = {
+  name:string,
+  quantity:number
+} & ComponentProps<typeof View>
+export const PlantMaterialCard = ({children, className, ...props} : PlantMaterialCardProps) => {
+  return <View className={twMerge("", className)} {...props}>{ children }</View>
 }
