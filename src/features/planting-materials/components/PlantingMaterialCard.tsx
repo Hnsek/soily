d606199cfc8f94a8fb431e123ba8bb360ee221ab
Hnsek <henrique2001sousa@gmail.com>
2026-01-30 15:30:08 -0300

    feat: implement function to format number in PlantingMaterialCard

--- src/features/planting-materials/components/PlantingMaterialCard.tsx
+++ src/features/planting-materials/components/PlantingMaterialCard.tsx
@@ -3,6 +3,10 @@ import { Card, CardContainer } from "../../../components/Card"
 import { Text } from "../../../components/Text"
 import { PlantingMaterial } from "../../../database/models/planting-material"
 
+const formatNumber = (number : number) => {
+  return String(number).replace(".", ",")
+}
+
 export type PlantingMaterialCardProps = {
   plantingMaterial: PlantingMaterial,
   onCreatePlanting: ( plantingMaterial : PlantingMaterial) => unknown
@@ -11,7 +15,7 @@ export const PlantingMaterialCard = ({plantingMaterial, onCreatePlanting} : Plan
   return <Card key={ plantingMaterial._id.toHexString()}>
           <CardContainer>
             <Text className="text-black font-bold">{plantingMaterial.name}</Text>
-            <Text className="text-black">{plantingMaterial.initialQuantity} KG</Text>
+            <Text className="text-black">{formatNumber(plantingMaterial.currentQuantity)} KG</Text>
           </CardContainer>  
           <Button onPress={() => onCreatePlanting(plantingMaterial)}>
             <Text>Plantar</Text>
