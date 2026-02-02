592ab51fad87360a2f37d7b8d9ac47db1a3a081e
Hnsek <henrique2001sousa@gmail.com>
2026-02-02 10:09:48 -0300

    chore: remove unused function in usePlantingMaterial

--- src/features/planting-materials/hooks/usePlantingMaterial.ts
+++ src/features/planting-materials/hooks/usePlantingMaterial.ts
@@ -4,32 +4,10 @@ import { CreatePlantingMaterial } from "../../../dtos/planting-materials"
 
 
 export const usePlantingMaterial = () => {
-  const realm = useRealm()
   const plantingMaterials = useQuery(PlantingMaterial)
-
     
-  const create = (plantingMaterial : CreatePlantingMaterial) => {
-    realm.write(() => {
-      realm.create(PlantingMaterial.schemaName, PlantingMaterial.generate(plantingMaterial))
-    })
-  }
-
-  const update = (id : Realm.BSON.UUID, plantingMaterial : Partial<PlantingMaterial>) => {
-    realm.write(() => {
-      const found = realm.objectForPrimaryKey(PlantingMaterial.schemaName, id)
-    
-      if(found){
-        Object.entries(plantingMaterial).forEach(([key,value]) => {
-          found[key] = value
-        })
-      }
-    })
-  }
-  
   return {
     plantingMaterials: plantingMaterials.toJSON() as unknown as PlantingMaterial[],
-    create,
-    update
   }
 
 }
