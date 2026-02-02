1cd6dd0ebfdf3896a300f9b60186795123afd368
Hnsek <henrique2001sousa@gmail.com>
2026-02-02 12:07:41 -0300

    chore: implement resetAction function in usePlantingMaterialAction

--- src/features/planting-materials/stores/usePlantingMaterialAction.ts
+++ src/features/planting-materials/stores/usePlantingMaterialAction.ts
@@ -7,11 +7,13 @@ type UsePlantingMaterial = {
   plantingMaterial: PlantingMaterial | null,
   action: Action | null
   setAction: (plantingMaterial: PlantingMaterial, action : Action) => unknown
+  resetAction: () => unknown
 }
 
 
 export const usePlantingMaterialAction = create<UsePlantingMaterial>((set) => ({
   plantingMaterial: null,
   action: null,
-  setAction:(plantingMaterial, action) => set((state) => ({ plantingMaterial, action}))
+  setAction:(plantingMaterial, action) => set((state) => ({ plantingMaterial, action})),
+  resetAction: () => set(() => ({ plantingMaterial: null, action: null}))
 }))
