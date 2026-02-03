096e1804271609b775844981653effb36e221c76
Hnsek <henrique2001sousa@gmail.com>
2026-02-03 09:27:09 -0300

    feat: implement data persist in CreatePlantingModal

--- src/features/plantings/components/CreatePlantingModal.tsx
+++ src/features/plantings/components/CreatePlantingModal.tsx
@@ -1,20 +1,31 @@
 import { Modal, ModalBackground } from "../../../components/Modal"
+import { usePersistPlantingMaterial } from "../../planting-materials/hooks/usePersistPlantingMaterial"
 import { usePlantingMaterialAction } from "../../planting-materials/stores/usePlantingMaterialAction"
+import { usePersistPlanting } from "../hooks/usePersistPlanting"
 import { PlantingForm } from "./PlantingForm"
 
 export const CreatePlantingModal = () => {
 
   const { plantingMaterial, action, resetAction } = usePlantingMaterialAction()
+  const { updatePlantingMaterial } = usePersistPlantingMaterial()
 
+  const { createPlanting } = usePersistPlanting()
+  
   const mayShow = !!plantingMaterial && action === "planting"
-
+  
   return <Modal visible={mayShow} transparent>
       <ModalBackground className="flex flex-col justify-end">
         { <PlantingForm 
           title={plantingMaterial?.name} 
           onClose={() => resetAction()}
           onSubmit={(planting) => {
+            if(!plantingMaterial){
+              return
+            }
 
+            createPlanting({...planting, plantingMaterial})
+            updatePlantingMaterial(plantingMaterial._id, plantingMaterial.plant(planting.quantity))
+            resetAction()
           }}/>
         } 
       </ModalBackground>
