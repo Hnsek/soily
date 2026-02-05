4e7e10027c4e389f62d3e179c79f84955405fce8
Hnsek <henrique2001sousa@gmail.com>
2026-02-05 17:32:23 -0300

    feat: implement list components in ListPlanting

--- src/features/plantings/ListPlanting.tsx
+++ src/features/plantings/ListPlanting.tsx
@@ -1,21 +1,13 @@
-import { Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
-import { Button } from "../../components/Button"
-import { BottomTabRouteList, StackRouteList } from "../../Router"
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { CompositeScreenProps } from "@react-navigation/native";
+import { BottomTabRouteList } from "../../Router"
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+import { PlantingList } from "./components/PlantingList";
+import { PlantingTitle } from "./components/PlantingTitle";
 
-type Props = CompositeScreenProps<
-  BottomTabScreenProps<BottomTabRouteList,"ListPlanting">,
-  NativeStackScreenProps<StackRouteList,"CreatePlanting">
->
+type Props = BottomTabScreenProps<BottomTabRouteList,"ListPlanting">;
 export const ListPlanting = ({navigation} : Props) => {
-  return <SafeAreaView className="h-screen w-screen bg-background">
-    <View className="w-full absolute bottom-0 p-4 flex items-center justify-center">
-      <Button className="w-full" onPressOut={() => navigation.navigate("CreatePlanting")}>
-        <Text className="text-white text-2xl">Registrar</Text>
-      </Button> 
-    </View> 
+  return <SafeAreaView className="h-screen w-screen bg-background p-4">
+    <PlantingTitle>Plantações</PlantingTitle>    
+    <PlantingList/>
   </SafeAreaView>
 }
