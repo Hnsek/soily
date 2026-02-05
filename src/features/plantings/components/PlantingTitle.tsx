5aabee8d00a5022689bf2892f8ac886fd9e539fe
Hnsek <henrique2001sousa@gmail.com>
2026-02-05 17:26:08 -0300

    feat: create PlantingTitle component

---
+++ src/features/plantings/components/PlantingTitle.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react"
+import { View } from "react-native"
+import { Text } from "../../../components/Text"
+
+type PlantingMaterialTitleProps = {
+  children?: ReactNode
+}
+export const PlantingTitle = ({ children } : PlantingMaterialTitleProps) =>{
+  return <View className="w-full py-6">
+    <Text className="text-black text-2xl font-bold">{ children }</Text>
+  </View>
+}
