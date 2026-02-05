00280412d8686e704f8e7091a040672aed002189
Hnsek <henrique2001sousa@gmail.com>
2026-02-05 17:58:29 -0300

    chore: remove unused folders

--- src/services/geospatial.ts
+++
@@ -1,6 +0,0 @@
-import { getPathLength as getPathLengthRoot } from "geolib"
-import { GeolibInputCoordinates } from "geolib/es/types"
-
-export const getPathLength = (points: GeolibInputCoordinates[]) => {
-  return getPathLengthRoot(points)
-}
--- src/services/share.ts
+++
@@ -1,5 +0,0 @@
-import Share, { ShareOptions} from "react-native-share";
-
-export const share = (options: ShareOptions) => {
-  return Share.open(options)
-}
