421cdb10d4f09adaedb59ac3e8d136f16bab514d
Hnsek <henrique2001sousa@gmail.com>
2026-02-06 10:54:40 -0300

    refactor: implement condition in CreateHarvestModal

--- src/features/harvest/components/CreateHarvestModal.tsx
+++ src/features/harvest/components/CreateHarvestModal.tsx
@@ -1,4 +1,5 @@
 import { Modal, ModalBackground } from "../../../components/Modal"
+import { usePersistPlanting } from "../../plantings/hooks/usePersistPlanting"
 import { usePersistHarvest } from "../hooks/usePersistHarvest"
 import { useHarvestAction } from "../stores/useHarvestAction"
 import { HarvestForm } from "./HarvestForm"
@@ -6,21 +7,21 @@ import { HarvestForm } from "./HarvestForm"
 export const CreateHarvestModal = () => {
 
   const { planting, action, resetAction } = useHarvestAction()
-
   const { createHarvest } = usePersistHarvest()
-  
+  const { updatePlanting } = usePersistPlanting()
+
   const mayShow = !!planting && action === "harvest"
   
   return <Modal visible={mayShow} transparent>
       <ModalBackground className="flex flex-col justify-end">
-        { <HarvestForm 
-          title={planting?.plantingMaterial.name} 
+      {planting && <HarvestForm 
+          title={planting.plantingMaterial.name} 
           onClose={() => resetAction()}
           onSubmit={(harvest) => {
-            createHarvest({...harvest, planting: planting!})
+            createHarvest({...harvest, planting})
+            updatePlanting(planting._id, {quantity: planting.harvest(harvest.quantity)})
             resetAction()
-          }}/>
-        } 
+          }}/>}
       </ModalBackground>
     </Modal>
 
