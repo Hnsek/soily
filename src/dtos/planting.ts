d7e3b0c4a4fafdbedff6a9c97ddd3cf7de6d9eab
Hnsek <henrique2001sousa@gmail.com>
2026-02-06 10:54:20 -0300

    fix: add omit of harvest in planting dto

--- src/dtos/planting.ts
+++ src/dtos/planting.ts
@@ -3,6 +3,6 @@ import { Realm } from '@realm/react'
 import { Planting } from '../database/models/planting'
 
 type PlantingKeys = Exclude<keyof Planting, keyof Realm.Object>
-type PlantingRemovedKeys = "createdAt" | "_id"
+type PlantingRemovedKeys = "createdAt" | "_id" | "harvest"
 
 export type CreatePlanting = Omit<Pick<Planting, PlantingKeys>, PlantingRemovedKeys>
