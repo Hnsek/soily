4f3a71cdb211874455593b556c1aa3c4d6290fcc
Hnsek <henrique2001sousa@gmail.com>
2026-02-06 10:43:30 -0300

    feat: implement CreateHarvestModal in ListPlanting

--- src/features/plantings/ListPlanting.tsx
+++ src/features/plantings/ListPlanting.tsx
@@ -3,11 +3,13 @@ import { BottomTabRouteList } from "../../Router"
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { PlantingList } from "./components/PlantingList";
 import { PlantingTitle } from "./components/PlantingTitle";
+import { CreateHarvestModal } from "../harvest/components/CreateHarvestModal";
 
 type Props = BottomTabScreenProps<BottomTabRouteList,"ListPlanting">;
 export const ListPlanting = ({navigation} : Props) => {
   return <SafeAreaView className="h-screen w-screen bg-background p-4">
     <PlantingTitle>Plantações</PlantingTitle>    
     <PlantingList/>
+    <CreateHarvestModal/>
   </SafeAreaView>
 }
